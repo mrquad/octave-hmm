b0822b77e2a7c5b1677811ab78d542db07012a86
Albertas Jurgelevicius <j.albertas@gmail.com>
2018-06-06 09:39:50 +0300

    Added data range parameters

--- run.sh
+++ run.sh
@@ -2,4 +2,4 @@
 
 set -e
 
-octave --eval "Recurrent_EM_hmm_skips_1gau(201, 300)" && cp hmm_with_skips_with_prob_total-2000_init-100.mat /root/shared/results/
+octave --eval "Recurrent_EM_hmm_skips_1gau($1, $2)" && cp hmm_with_skips_with_prob_total-2000_init-100.mat /root/shared/results/
